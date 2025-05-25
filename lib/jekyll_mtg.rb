ffc4884798cd40d2ba027bf2dbcde6b529e861dc
Matthew Serre <matthew.serre+github@gmail.com>
2025-05-25 13:56:43 -0400

    Set up gem Add logic for linking card

---
+++ lib/jekyll_mtg.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'jekyll'
+require 'net/http'
+require_relative 'jekyll_mtg/version'
+
+module JekyllMtg
+  # Links cards
+  class LinkCard < Liquid::Tag
+    REQUEST_FORMAT = 'json'
+    SCYRFALL_URI = 'https://api.scryfall.com/'
+    SCRYFALL_PATH = '/cards/named'
+
+    def initialize(tag_name, card_info, tokens)
+      super
+
+      @card_info = card_info
+    end
+
+    def render(_context)
+      parts = card_info.split('|')
+      card_name = parts[0]
+      set_code = parts[1]
+
+      uri = URI(SCYRFALL_URI)
+      uri.path = SCRYFALL_PATH
+      params = { fuzzy: card_name, set: set_code, format: REQUEST_FORMAT }
+      uri.query = URI.encode_www_form(params)
+      response = Net::HTTP.get(uri)
+      response = JSON.parse(response)
+
+      "<a href='#{response['scryfall_uri']}>#{card_name}</a>"
+    end
+
+    Liquid::Template.register_tag 'link_card', self
+  end
+end
---
+++ spec/jekyll_mtg_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JekyllMtg do
+  it "has a version number" do
+    expect(JekyllMtg::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
