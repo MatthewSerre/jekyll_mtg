fd691c270dd1882a4c0883b9ae60544acca314cf
Matthew Serre <matthew.serre+github@gmail.com>
2025-05-26 13:26:23 -0400

    Improve specs Bump version

--- spec/jekyll_mtg_spec.rb
+++ spec/jekyll_mtg_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe JekyllMtg do
     expect(JekyllMtg::VERSION).not_to be nil
   end
 
+  def extract_link(contents, card_name)
+    card_link_regex = %r{<a href='([^']+)'[^>]*?>#{card_name}</a>}
+    contents.match(card_link_regex)
+  end
+
+  def generate_regex_for_expected(card_name)
+    %r{^https://scryfall\.com/card/[a-zA-Z0-9]+/\d+/#{card_name.downcase.gsub(" ", "-")}\?utm_source=api$}
+  end
+
   context 'LinkCard' do
     let(:config_overrides) { {} }
     let(:config) do
@@ -25,7 +34,9 @@ RSpec.describe JekyllMtg do
     end
 
     it 'links the card from a non-specific set when only the name ("Ajani Goldmane") is provided' do
-      expect(contents).to include("<a href='https://scryfall.com/card/m11/1/ajani-goldmane?utm_source=api'>Ajani Goldmane</a>")
+      match = extract_link(contents, 'Ajani Goldmane')
+      expect(match).not_to be_nil
+      expect(match[1]).to match(generate_regex_for_expected('Ajani Goldmane'))
     end
 
     it 'links the card from a specific set when the name ("Jace Beleren") and set code ("LRW") are provided' do
@@ -33,23 +44,31 @@ RSpec.describe JekyllMtg do
     end
 
     it 'does not link the card when the name ("Liliana Vess") and set code ("POO") are provided and the set code does not exist' do
+      match = extract_link(contents, 'Liliana Vess')
       expect(contents).to include('Liliana Vess')
+      expect(match).to be(nil)
     end
 
     it 'does not link the card when the name ("Chandra Nalaar") and set code ("MOR") are provided and the set code is not one in which the card appeared' do
+      match = extract_link(contents, 'Chandra Nalaar')
       expect(contents).to include('Chandra Nalaar')
+      expect(match).to be_nil
     end
 
-    it 'links the card ("Garruk Wildspeaker") when the name is misspelled slightly ("Garuk Wildspeeker")' do
-      expect(contents).to include("<a href='https://scryfall.com/card/lrw/213/garruk-wildspeaker?utm_source=api'>Garuk Wildspeeker</a>")
+    it 'links the card when the name ("Garruk Wildspeaker") is misspelled slightly ("Garuk Wildspeeker")' do
+      match = extract_link(contents, 'Garuk Wildspeeker')
+      expect(match).not_to be_nil
+      expect(match[1]).to match(generate_regex_for_expected('Garruk Wildspeaker'))
     end
 
     it 'does not link the card when the name ("Elspeth, Knight-Errant") is misspelled significantly ("Elspooth, Nite Errand")' do
-      expect(contents).to include('Elspooth, Nite Errand')
+      match = extract_link(contents, 'Elspooth, Nite Errand')
+      expect(match).to be_nil
     end
 
-    it 'does not link the card when the name ("Mickey Mouse") does not exist' do
-      expect(contents).to include('Mickey Mouse')
+    it 'does not link the card when the name ("Mickey Mouse") does not match the name of an existing card' do
+      match = extract_link(contents, 'Mickey Mouse')
+      expect(match).to be_nil
     end
   end
 end
