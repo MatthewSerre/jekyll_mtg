be9aa42660dadb7c0737afdd3767a253a30b6f9a
Matthew Serre <matthew.serre+github@gmail.com>
2025-05-25 20:26:05 -0400

    Add headers to API calls Bump version

--- lib/jekyll_mtg.rb
+++ lib/jekyll_mtg.rb
@@ -20,15 +20,13 @@ module JekyllMtg
     attr_reader :card_info
 
     def render(_context)
-      parts = card_info.split('|')
-      card_name = parts[0]
-      set_code = parts[1]
-
+      card_name, set_code = card_info.split('|')
       uri = URI(SCYRFALL_URI)
       uri.path = SCRYFALL_PATH
       params = { fuzzy: card_name, set: set_code, format: REQUEST_FORMAT }
       uri.query = URI.encode_www_form(params)
-      response = Net::HTTP.get(uri)
+      headers = { 'Accept' => 'application/json', 'User-Agent' => "JekyllMTG/#{JekyllMTG::VERSION}" }
+      response = Net::HTTP.get(uri, headers)
       response = JSON.parse(response)
 
       "<a href='#{response["scryfall_uri"]}'>#{card_name}</a>"
